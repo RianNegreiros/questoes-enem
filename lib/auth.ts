918a10c9982ffe6e9165b7a1f6b5aebe8dd9cd81
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-06-22 10:55:47 -0300

    chore: config social providers

--- lib/auth.ts
+++ lib/auth.ts
@@ -7,4 +7,14 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    facebook: {
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FABOOK_CLIENT_SECRET as string,
+    },
+  },
 })
